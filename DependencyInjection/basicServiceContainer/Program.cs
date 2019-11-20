72d7fa21cfa7f56e27cf47b8361bde2757276ac1
jctechhub <jcdsta@gmail.com>
2019-11-21 10:06:58 +1100

    DI basic sample: service container in console app

---
+++ DependencyInjection/basicServiceContainer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace basicServiceContainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+               var collection = new ServiceCollection();
+            collection.AddScoped<IDataAccess, APIDataAccess>();
+            var provider = collection.BuildServiceProvider();
+
+            var data = provider.GetService<IDataAccess>();
+            Console.WriteLine(data.GetType());
+        }
+    }
+
+    public interface IDataAccess { }
+
+    public class DBDataAccess : IDataAccess
+    {
+
+    }
+
+    public class APIDataAccess : IDataAccess
+    {
+
+    }
+
+
+
+}
